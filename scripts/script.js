a35b75736e76db996280fc67df44777e9abecf22
King-Normagomadov <104327135+King-Normagomadov@users.noreply.github.com>
2023-12-10 13:27:46 +0100

    10-09

--- scripts/script.js
+++ scripts/script.js
@@ -66,17 +66,6 @@ function stext(x){
       }
 }
 
-//menue on click open and close the menuebar
-let mspan = document.getElementById("mspan")
-let m1 = document.getElementById("m1")
-function mspan1(){
-    if(m1.innerHTML==""||m1.innerText==""){
-        m1.innerHTML='<div class="divm11"><a href="./index.html">Home</a></div><div class="divm12"><a href="./pages/produkt.html">Produkte</a></div><div class="divm13"><a href="./pages/dienstleistung.html">Diestleistungen</a></div><div class="divm13"><a href="./pages/shop.html">Shop</a></div><div class="divm14"><a href="./pages/about.html">About us</a></div>'
-    }else{
-        m1.innerHTML=null;
-    }
-}
-mspan.addEventListener("click",mspan1)
 
     
 
