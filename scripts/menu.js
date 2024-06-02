442c52cbcdb2f1a1dd653c75d0291e85a3a4ba6e
King-Normagomadov <104327135+King-Normagomadov@users.noreply.github.com>
2024-06-02 17:43:52 +0200

    6-2-24

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,17 +1,17 @@
 //menue on click open and close the menuebar smart phone
-let mspan = document.getElementById("mspan")
+let mbar = document.getElementById("mbar")
+let rows = document.getElementsByClassName("mrows")
 let m1 = document.getElementById("m1")
+
 function mspan1(){
-    if(m1.innerHTML==""||m1.innerText==""){
-       // mspan.style.backgroundColor="rgba(0, 140, 255)";
-        //mspan.style.color="white";
-        mspan.innerHTML="&#10006;"
-        m1.innerHTML='<div class="divm11"><a href="https://itwurzel.de">Home</a></div><div class="divm12"><a href="https://itwurzel.de/pages/produkt.html">Produkte</a></div><div class="divm13"><a href="https://itwurzel.de/pages/dienstleistung.html">Diestleistungen</a></div><div class="divm13"><a href="https://itwurzel.de/pages/shop.html">Shop</a></div><div class="divm14"><a href="https://itwurzel.de/pages/about.html">About us</a></div>'
-    }else{
-        m1.innerHTML=null;
-       // mspan.style.backgroundColor="white";
-       // mspan.style.color="rgba(0, 140, 255)";
-        mspan.innerHTML="&#10010;"
-    }
+if(m1.innerHTML==""||m1.innerText==""){
+rows[0].style.visibility = "hidden";
+rows[2].style.visibility = "hidden"; 
+m1.innerHTML='<div class="divm11"><a href="https://itwurzel.de">Home</a></div><div class="divm12"><a href="https://itwurzel.de/pages/produkt.html">Produkte</a></div><div class="divm13"><a href="https://itwurzel.de/pages/dienstleistung.html">Diestleistungen</a></div><div class="divm13"><a href="https://itwurzel.de/pages/shop.html">Shop</a></div><div class="divm14"><a href="https://itwurzel.de/pages/about.html">About us</a></div>'
+}else{
+m1.innerHTML=null;
+rows[0].style.visibility = "visible";
+rows[2].style.visibility = "visible"; 
 }
-mspan.addEventListener("click",mspan1)
+}
+mbar.addEventListener("click",mspan1)
